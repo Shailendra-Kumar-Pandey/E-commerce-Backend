20912c3b922eb00fd5b503be4642f68eca161a19
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-20 01:18:57 +0530

    Add more  models

---
+++ src/models/SallerProfile.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const sallerProfileSchema = new mongoose.Schema(
+    {
+        userId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'user'
+        },
+        panNumber:{
+            type:String,
+            trim:true,
+            minlength:11,
+            required:true
+        },
+        aadharNumber:{
+            type:number,
+            minlength:12,
+            required: true,
+        },
+        gstNumber:{
+            type: String,
+            trim: true,
+            required: true
+        },
+        address:{
+            type:String,
+            trim:true,
+            required:true
+        },
+        status:{
+            type: String,
+            enum:['Pandding','Approved','Rejected'],
+            default:'Pandding'
+        },
+        adminRemark:{
+            type: String,
+            trim: true
+        },
+        approvedAT:{
+            type: Date
+        }
+
+    },
+    {
+        timestamps:true
+    }
+)
+
+export default ('sallerProfile', sallerProfileSchema );
--- src/models/User.js
+++ src/models/User.js
@@ -9,10 +9,37 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     email:{
-        ty
+        type:String,
+        required: true,
+        trim : true,
+        unique: true
+    },
+    phone:{
+        type: String,
+        required: true,
+        trim:true,
+        minlength:10
+    },
+    password:{
+        type: String,
+        trim:true,
+        minlength:6,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum : ['Admin','Customar','Saller'],
+        required: true
+    },
+    isActive:{
+        type:Boolean,
+        default:true
     }
   },
   {
     timestamps: true,
   },
 );
+
+
+export default mongoose.model('user', userSchema);
