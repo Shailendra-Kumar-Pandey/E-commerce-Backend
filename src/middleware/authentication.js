64097c6bf5d6ef450e2cdcb7fdc43495a638b9a5
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-23 17:40:25 +0530

    add application  Middleware...

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,9 +1,28 @@
+import jwt from 'jsonwebtoken';
 
 const authentication = async (req, res, next)=>{
 
-    let authData = req.headers.authentication
+    let authData = req?.headers?.authorization
 
-    return
+    try {
+        if(!authData && !authData?.startsWith('Bearer')){
+            return res.status(404).json({massage: "Unauthorization User... Please Enter Token..."})
+        }
+
+        let token = authData?.startsWith('Bearer')[1]
+
+        let decode = jwt?.verify(token, process.env.SECRET_TOKEN);
+
+        if(!decode){
+            return res.status(404).json({massage : "Please Enter Vailid Token... Unauthorization User..."})
+        }
+
+        next()
+
+        return
+    } catch (error) {
+        return res.status(500).json({massage:`${error}, Server Error...`})
+    }
 
 }
 
